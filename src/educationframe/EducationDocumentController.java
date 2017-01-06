c82d9af3481d2a521150819bed675e4a3dd6f360
letam <letam210795@gmail.com>
2017-01-06 18:16:15 +0700

    edit login & register. move img folder to right place

--- src/educationframe/EducationDocumentController.java
+++ src/educationframe/EducationDocumentController.java
@@ -14,6 +14,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -21,6 +22,9 @@ import javafx.scene.control.Label;
  */
 public class EducationDocumentController implements Initializable {
 
+    @FXML
+    private ImageView imgLogin;
+    
     @FXML
     private JFXTextField tfUsername;
 
